33da8f3da3aa3cb14eddc5801c67cf0d59824e95
matthew enbar <matthew.enbar@gmail.com>
2020-03-17 23:02:56 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,41 +1,20 @@
 function basicTeenager(age) {
 if (age > 12 && age< 20)
-return "You are a teenager!"
+return true
 }
 
 function teenager(age) {
-if (age > 12 && age < 20){
-return 'You are a teenager!'
-}
-else return 'You are not a teenager'
+
 }
 
 function ageChecker(age) {
-  if (age < 13 ){
-    return 'You are a kid'
-  }
-  if else (age > 12 && age < 20){
-    return 'You are a teenager!'
-  }
-  if else (age > 19) {
-    return 'You are a grownup'
-  }
 
 }
 
 function ternaryTeenager(age) {
-  if (age > 12 && age < 20){
-    return "You are a teenager"
-  }
-  if else (age < 13 || age > 20) {
-    return "You are not a teenager"
-  }
+
 }
 
 function switchAge(age) {
-if (age > 12 && age < 20){
-return "You are a teenager"}
-if else (age < 13 || age > 20) {
-  return "You have an age"
-}
+
 }
